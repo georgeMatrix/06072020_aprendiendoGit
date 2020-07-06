5984d40d415f097739e0649a519509a737572fac
GeorgeX <jorgenietom83@gmail.com>
2020-07-06 17:51:24 -0500

    se ponen el servicio y la impl del padre

---
+++ src/main/java/com/formatoweb/git06072020/services/PadreService.java
@@ -0,0 +1,9 @@
+package com.formatoweb.git06072020.services;
+
+import com.formatoweb.git06072020.entity.Padres;
+
+import java.util.List;
+
+public interface PadreService {
+    List<Padres> getPadres();
+}
---
+++ src/main/java/com/formatoweb/git06072020/services/impl/PadreServiceImpl.java
@@ -0,0 +1,19 @@
+package com.formatoweb.git06072020.services.impl;
+
+import com.formatoweb.git06072020.entity.Padres;
+import com.formatoweb.git06072020.repository.PadreRepository;
+import com.formatoweb.git06072020.services.PadreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class PadreServiceImpl implements PadreService {
+    @Autowired
+    private PadreRepository padreRepository;
+
+    @Override
+    public List<Padres> getPadres() {
+        return padreRepository.findAll();
+    }
+}
