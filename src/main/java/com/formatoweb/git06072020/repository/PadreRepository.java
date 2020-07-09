8e1812c4b704adea3cb9544f63efa1f7bb90931b
GeorgeX <jorgenietom83@gmail.com>
2020-07-09 15:48:04 -0500

    Revert "Revert "cambios: PadreController, PadreService""
    This reverts commit 5a8e62ee

--- src/main/java/com/formatoweb/git06072020/repository/PadreRepository.java
+++ src/main/java/com/formatoweb/git06072020/repository/PadreRepository.java
@@ -2,10 +2,8 @@ package com.formatoweb.git06072020.repository;
 
 import com.formatoweb.git06072020.entity.Padres;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.io.Serializable;
 
-@Repository
 public interface PadreRepository extends JpaRepository<Padres, Serializable> {
 }
